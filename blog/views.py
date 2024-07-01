3e721d7548f6d918ac8420bdb5a40fbd71391bcf
Bruno-Pierre Bouchard <bbouchar@c2r2p7.42quebec.com>
2024-07-01 16:44:31 -0400

    Added database backend

--- blog/views.py
+++ blog/views.py
@@ -1,87 +1,27 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-posts_items = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in te mountains! And I wasn't even prepared for what happened whilst I was enjoying the views !",
-        "content": """ 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus convallis dapibus consectetur. Suspendisse et congue ante, at aliquet nisl. Nunc sed nulla vitae enim gravida egestas.
-            Aenean vestibulum feugiat luctus. Aliquam congue ornare metus, vitae scelerisque nunc posuere eu. Vestibulum tristique enim sed nulla elementum,
-            eu dapibus magna porttitor. Praesent cursus lacinia dui at tincidunt. Duis tincidunt vitae sem vel ornare. 
-            Suspendisse eleifend turpis ac sapien cursus vestibulum. 
-            Proin dapibus laoreet magna feugiat laoreet. 
-            Pellentesque in ultrices arcu.
-            Ut suscipit lectus velit, quis dapibus enim gravida non."
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+from .models import Post
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
 def get_date(post):
     return post.get('date')
 
 # Create your views here.
 def starting_page(request):
-    sorted_posts = sorted(posts_items, key=get_date, reverse=True)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
+    latest_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
-        "all_posts": posts_items
+        "all_posts": latest_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in posts_items if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
